f20f0aa670d1e1663fb5e11b59a67d4aa7b2d8c4
cornerstonian <vois.cornerstone@gmail.com>
2016-06-30 15:40:14 -0500

    can create a book test

--- test/features/create_update_and_delete_books_test.rb
+++ test/features/create_update_and_delete_books_test.rb
@@ -8,7 +8,6 @@ class CreateUpdateAndDeleteBooksTest < Capybara::Rails::TestCase
     Author.create first_name: "Jane", last_name: "Fonda", bio: "The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, making it look like readable English."
     Author.create first_name: "Miles", last_name: "Munroe", bio: "There are many variations of passages of Lorem Ipsum available"
     Book.create(title: "Ghost In The Wires", photo_url: 'http://buff.ly/29ao1SS', price: '21' )
-
   end
 
   test "Can view author detail" do
@@ -23,6 +22,24 @@ class CreateUpdateAndDeleteBooksTest < Capybara::Rails::TestCase
       click_link "Books"
       click_link "Ghost In The Wires"
       assert_content page, "21"
+  end
+
+  test "Can add a book" do
+    visit root_path
+    click_link "Add A Book"
+
+    fill_in "Title", with: "Ghost In The Wires"
+    fill_in "Price", with: "21"
+    fill_in "Photo url", with: "http://buff.ly/293HX4m"
+    click_button "Save Book"
+
+    assert_content page, "Ghost"
+    assert_content page, "New Book Added to the Collection"
+    book = Book.find_by title: "Ghost In The Wires"
+    assert book, "Book should exist after filling out form"
+
+    assert_equal "Ghost In The Wires", book.title
+
   end
 
 end
